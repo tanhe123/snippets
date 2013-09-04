9d5b1c9fef01ac47a015882767bb3f3b5b10175a
tanhehe <443016215@qq.com>
2013-09-04 14:56:21 +0800

    add atof.c

---
+++ atof.c
@@ -0,0 +1,33 @@
+#include <ctype.h>
+#include <stdio.h>
+
+double atof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i]-'0');
+    if(s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
+
+int atoi(char s[])
+{
+    double atof(char s[]);
+    return (int) atof(s);
+}
+
+int main(){
+    printf("%d\n", atoi("3121"));
+    return 0;
+}
